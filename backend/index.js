910ac4d6573e74b4147d57ce9a66b53d98bc3007
Riya813 <Riyasingh813@gmail.com>
2022-04-05 23:09:36 +0530

    Tuesday Update

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded());
+app.use(express.static('assets'));
 
 var toDoList = [
     // {
@@ -23,13 +24,41 @@ var toDoList = [
 ]
 
 app.get('/', function(req, res){
-    // res.send('<h1>Hello</h1>');
 
     return res.render('home', {
         title: "My To-Do List",
         todo_List: toDoList
     });
 })
+
+// app.get('/delete-task/', function(req, res){
+//     console.log(req.query);
+    
+    
+// })
+var complete = ["finish jquery"]
+
+app.get('/removetask', function(req, res){
+    var deleteTask = req.body.check;
+    let taskIndex = toDoList.findIndex(task => task.deleteTask == deleteTask);
+    
+    if(taskIndex != -1){
+        toDoList.splice(taskIndex, 1);
+    }
+
+    return res.redirect('back');
+})
+
+// app.get('/delete-btn/', function(req, res){
+//     let date = req.query.date;
+//     let taskIndex = toDoList.findIndex(task => task.date == date);
+    
+//     if(taskIndex != -1){
+//         toDoList.splice(taskIndex, 1);
+//     }
+
+//     return res.redirect('back');
+// })
 
 app.post('/create-task', function(req, res){
     // toDoList.push({
